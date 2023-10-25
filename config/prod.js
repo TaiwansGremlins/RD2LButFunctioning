f5bb4f616c4e6800467fc752b5771e7d12306849
West Taiwan <emrickgarrett@gmail.com>
2023-10-24 23:15:18 -0400

    Add opendota api keys, as a proof of concept before working with valves obscure documentation

--- config/prod.js
+++ config/prod.js
@@ -1,5 +1,6 @@
 module.exports = {
-	mongoRoute = process.env.MONGO_ROUTE,
-	cookieKey = process.env.COOKIE_KEY,
-	redirectDomain: process.env.REDIRECT_DOMAIN
+	mongoRoute: process.env.MONGO_ROUTE,
+	cookieKey: process.env.COOKIE_KEY,
+	redirectDomain: process.env.REDIRECT_DOMAIN,
+	openDotaAPI: process.env.OPEN_DOTA_API,
 }
