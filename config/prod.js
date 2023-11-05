095518c4d5e32cba20a917585770f2b083b98d30
West Taiwan <emrickgarrett@gmail.com>
2023-11-04 20:22:11 -0400

    added stratz api key entry, will likely switch over to their API

--- config/prod.js
+++ config/prod.js
@@ -3,5 +3,6 @@ module.exports = {
 	cookieKey: process.env.COOKIE_KEY,
 	redirectDomain: process.env.REDIRECT_DOMAIN,
 	openDotaAPI: process.env.OPEN_DOTA_API,
-	steamAPI: process.env.STEAM_API
+	steamAPI: process.env.STEAM_API,
+	stratzAPI: process.env.STRATZ_API
 }
